11cb88348427afb0b4d0e56e108d25e1ba5df4b5
Michael Davis <mkizziah38@gmail.com>
2013-09-23 19:50:32 -0500

    First comment.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capybaratestform::Application.config.secret_token = '37dac2535cff18d82c6e6b1e11fd06d9635605504f8eaf8d2f6f5eec501ad83d4a759f93c6722f03062d83e202c433cc1bd47f47837b9a71b571df12da3da876'
